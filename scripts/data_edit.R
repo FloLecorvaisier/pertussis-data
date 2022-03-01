7663b0840a2cd372be1d24e15a1e0531d8f7007c
Florian Lecorvaisier <florian.lecorvaisier@gmail.com>
2022-03-01 09:55:26 +0100

    Added data from Hiramatsu et al., 2017 (Japan)

--- scripts/data_edit.R
+++ scripts/data_edit.R
@@ -4,7 +4,40 @@
 library(openxlsx)
 library(readxl)
 
-# Import the raw .xlsx table from Schmidtke et al., 2012
+#### Data: Hiramatsu et al., 2017 ####
+
+# Data provided by the authors in a PDF form, thus the TXT file had to be created manually by converting the PDF to an XLSX file using ilovepdf.com then editing the output to have a clean TXT file
+
+# Import the TXT data frame
+data_jp <- read.table("data/16-1575-TECHAPP1.txt", header = T, sep = "\t")
+
+# Formate it
+for (x in 1:nrow(data_jp)) {
+  if (data_jp$prn.allele[x] == "ND") {
+    data_jp$prn.allele[x] = NA
+  }
+  
+}
+
+# Create the final data frame
+data_jp <- data.frame(strain_id = data_jp$Isolate,
+                      year_isolated = data_jp$Isolation.year,
+                      country = "Japan",
+                      region = data_jp$Origin..district.,
+                      prn = data_jp$prn.allele,
+                      prn_def = data_jp$Pertactin.production,
+                      ptxP = NA,
+                      ptxA = NA,
+                      fim2 = NA,
+                      fim3 = NA,
+                      FIM_sero = NA)
+
+# Export it
+write.table(data_jp, file = "data/data_jp.txt", quote = F, sep = "\t")
+
+#### Data: Schmidtke et al., 2012 ####
+
+# Import the raw XLSX table
 data_us <- as.data.frame(read.xlsx("https://wwwnc.cdc.gov/eid/article/18/8/12-0082-techapp1.xlsx"))
 
 # Clean it
@@ -30,8 +63,9 @@ data_us <- data.frame(strain_id = data_us$`Strain Number`,
 # Export it
 write.table(data_us, file = "data/data_us.txt", quote = F, sep = "\t")
 
+#### Data: Dakic et al., 2010 ####
 
-# Import data from Dakic et al, 2010. Note that the data is not open-access and thus cannot be directly downloaded
+# Import data from the XLS file. Note that the data is not open-access and thus cannot be directly downloaded
 data_serbia <- as.data.frame(read_excel("data/1-s2.0-S0264410X09018118-mmc1.xls"))
 
 # Clean it
